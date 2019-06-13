be559ee2da189c63440ebc6a56b85452b96650a7
devJake <devjake@outlook.com>
2019-06-13 08:04:11 -0600

    minor tweaks

--- Destini/ViewController.swift
+++ Destini/ViewController.swift
@@ -34,31 +34,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var bottomButton: UIButton!      // Has TAG = 2
     @IBOutlet weak var storyTextView: UILabel!
     
-    // TODO Step 5: Initialise instance variables here
-    var T1_Story: String = ""
-    var T1_Ans1: String = ""
-    var T1_Ans2: String = ""
-    
     var currentStory: Int = 1
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        // TODO Step 3: Set the text for the storyTextView, topButton, bottomButton, and to T1_Story, T1_Ans1, and T1_Ans2
-        //storyTextView.text = story1
-        //topButton.setTitle(answer1a, for: .normal)
-        //bottomButton.setTitle(answer1b, for: .normal)
-        
         //** start off with first story and answers **
-        T1_Story = story1
-        T1_Ans1 = answer1a
-        T1_Ans2 = answer1b
-        updateUI(displayStory: T1_Story, topBtnAnswer: T1_Ans1, botBtnAnswer: T1_Ans2)
         updateUI(displayStory: story1, topBtnAnswer: answer1a, botBtnAnswer: answer1b)
-        
         
     }
 
@@ -68,53 +51,47 @@ class ViewController: UIViewController {
     
         // TODO Step 4: Write an IF-Statement to update the views
         
+        
         switch currentStory {
         case 1:
             if sender.tag == 1 {
                 print("You selected top button!")
-                updateUI(displayStory: story3, topBtnAnswer: answer3a, botBtnAnswer: answer3b)
                 currentStory = 3
+                updateUI(displayStory: story3, topBtnAnswer: answer3a, botBtnAnswer: answer3b)
                 
             } else if sender.tag == 2 {
                 print("You selected bottom button!")
-                updateUI(displayStory: story2, topBtnAnswer: answer2a, botBtnAnswer: answer2b)
                 currentStory = 2
+                updateUI(displayStory: story2, topBtnAnswer: answer2a, botBtnAnswer: answer2b)
             }
             break
         case 2:
             //TODO which button and load story3 or story4
             if sender.tag == 1 {
                 print("You selected top button!")
-                //TODO update ui - story 3
-                updateUI(displayStory: story3, topBtnAnswer: answer3a, botBtnAnswer: answer3b)
                 currentStory = 3
+                updateUI(displayStory: story3, topBtnAnswer: answer3a, botBtnAnswer: answer3b)
             } else if sender.tag == 2 {
                 print("you selected bottom button!")
-                //TODO update ui - story 4
-                updateUI(displayStory: story4, topBtnAnswer: "-", botBtnAnswer: "-")
                 currentStory = 4
+                updateUI(displayStory: story4, topBtnAnswer: "-", botBtnAnswer: "-")
             }
             break
         case 3:
             if sender.tag == 1 {
                 print("You selected top button!")
-                updateUI(displayStory: story6, topBtnAnswer: "-", botBtnAnswer: "-")
                 currentStory = 6
+                updateUI(displayStory: story6, topBtnAnswer: "-", botBtnAnswer: "-")
             } else if sender.tag == 2 {
                 print("You selected bottom button!")
-                updateUI(displayStory: story5, topBtnAnswer: "-", botBtnAnswer: "-")
                 currentStory = 5
+                updateUI(displayStory: story5, topBtnAnswer: "-", botBtnAnswer: "-")
             }
             break
         default: break
             
         }
         
-        
-        
-        
-        
-                
         // TODO Step 6: Modify the IF-Statement to complete the story
         
     
@@ -124,6 +101,7 @@ class ViewController: UIViewController {
         storyTextView.text = displayStory
         topButton.setTitle(topBtnAnswer, for: .normal)
         bottomButton.setTitle(botBtnAnswer, for: .normal)
+        print("loaded \(currentStory)")
     }
     
 
